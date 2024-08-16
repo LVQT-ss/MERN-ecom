ac4b69bc317e9f80f320385332c7df650b8c675c
LVQT <levietquocthinh@gmail.com>
2024-08-16 22:42:47 +0700

    start Server nodejs

---
+++ frontend/src/components/homeComponents/ShopSection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ShopSection = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='section'>
+                <div className='row'>
+                    <div className='col-lg-12 col-md-12 article'>
+                        <div className='shopcontainer row'>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div> 
+    </>
+  )
+}
+export default ShopSection
